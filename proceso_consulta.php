a6bc93e337acce730bb1af1b22eff0e4c28a729d
irevios <irevios@gmail.com>
2019-11-19 22:10:07 +0100

    Version 2

--- proceso_consulta.php
+++ proceso_consulta.php
@@ -8,16 +8,30 @@ extract ($_POST);
 $conexion=new mysqli("localhost","root","","ejerClase");
 $conexion->set_charset("utf8");
 
-// Cojo los datos de los usuarios
+// Coge los datos de los usuarios
 $sqlPrincipal="select num_orden,nombre from encuesta";
 $resultPrincipal=$conexion->query($sqlPrincipal);
  	$sEntra=false;
 echo "<h3>Lenguaje: ".((isset($progLeng)) ? $progLeng:"Todos").", Idioma: ".((isset($idioma)) ? $idioma:"Todos")."</h3>";
 
 $cabecera="<table border='1'><tr><th>Nombre</th><th>Lenguajes</th><th>Idiomas</th>";
+
+
+// De los datos consultados, devuelve si cumple:
+function comprueba($datos,$elegido){
+		$bool = false;
+		$resultado = "";
+		if($elegido==""){$bool=true;} // Se ha elegidos todos
+		while ($fila=$datos->fetch_assoc())
+		{
+			$resultado.=$fila["nombre"]."<br>";
+			if (isset($elegido) && $fila["nombre"]==$elegido){$bool=true;} // Tiene el que se pide
+		}
+		return [$bool,$resultado]; // Devuelve si tiene el lenguaje/idioma seleccionado y sus lenguajes/idiomas.
+}
+
 // Por cada usuario
-while ($fila=$resultPrincipal->fetch_assoc())
-	{
+while ($fila=$resultPrincipal->fetch_assoc()){
 	extract($fila);
 	$datosEncuestado="<tr><td>".$nombre."</td><td>";
 	
@@ -26,58 +40,20 @@ while ($fila=$resultPrincipal->fetch_assoc())
 	
 	$sqlIdiomas="SELECT nombre from idiomas where id_idioma in (SELECT id_idioma from habla where num_orden = $num_orden)";
 	$resultIdiomas=$conexion->query($sqlIdiomas);
-	$estaLenguaje=false;
-	$estaIdioma=false;
-	$datosLenguaje="";
-	$datosIdioma="";
 
-	// Comprobar lenguaje de Programación
-	if(!isset($progLeng)){
-		$estaLenguaje=true;
-	}
-	while ($filaLenguajes=$resultLenguajes->fetch_assoc())
-	{
-		$datosLenguaje.=$filaLenguajes["nombre"]."<br>";
-		
-		if (isset($progLeng))
-		{
-			if ($filaLenguajes["nombre"]==$progLeng)
-		       {$estaLenguaje=true;}
-		}
-		else
-		{
-			$estaLenguaje=true;
-		}
-	}
-	
+	// Coge sus lenguajes y dime si tiene el que se pide:
+	$leng = comprueba($resultLenguajes,isset($progLeng)?$progLeng:"");
+	$estaLenguaje=$leng[0]; // ¿Tiene el que se pide? (true/false)
+	$datosLenguaje=$leng[1]; // Lenguajes del usuario
 
+	// Coge sus idiomas y dime si tiene el que se pide:
+	$idiom = comprueba($resultIdiomas,isset($idioma)?$idioma:"");
+	$estaIdioma=$idiom[0];  // ¿Tiene el que se pide? (true/false)
+	$datosIdioma=$idiom[1]; // Idiomas del usuario
 
-	//comprobar idiomas
-	if(!isset($idioma)){
-		$estaIdioma=true;
-	}
-	while ($filaIdiomas=$resultIdiomas->fetch_assoc())
-	{
-		$datosIdioma.=$filaIdiomas["nombre"]."<br>";
-		
-		 if (isset($idioma))
-		 {
-		 	if ($filaIdiomas["nombre"]==$idioma)
-		        {$estaIdioma=true;}
-		 }
-		 else
-		 {
-		$estaIdioma=true;
-		}
-	}
-	
-
-	if ($estaLenguaje&&$estaIdioma)
-	{
-		if (!$sEntra){
-			echo $cabecera;
-			$sEntra=true;
-		}
+	// Muestralos
+	if ($estaLenguaje&&$estaIdioma){
+		if (!$sEntra){echo $cabecera;$sEntra=true;}
 		echo $datosEncuestado.$datosLenguaje."</td><td>".$datosIdioma."</td></tr>";
 	}
 		
@@ -85,7 +61,7 @@ while ($fila=$resultPrincipal->fetch_assoc())
 	if ($sEntra){
 		echo "</table>";
 	}
-	else{
+	else{ // Validaciones 
 		if(isset($idioma)&&isset($progLeng)){
 			echo "Ningún candidato conoce $progLeng ni habla $idioma";
 		}
