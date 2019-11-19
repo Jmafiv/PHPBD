64962cffd96ddbd6e4cb94fdb5e7bcb17e58cd93
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-11-19 10:17:16 +0100

    Update proceso_insercion.php

--- proceso_insercion.php
+++ proceso_insercion.php
@@ -2,8 +2,34 @@
 include('funciones.php');
 cabecera('Examen PHP');
 echo "<div id=\"contenido\">\n";
-echo "<h2>Aquí los Inserts</h2>";
+//echo "<h2>Aquí los Inserts</h2>";
+extract ($_POST);
+$conexion=mysqli_connect ("localhost","root","","ejerClase");
+$sql="INSERT INTO encuesta(nombre) VALUES ('$nombre')";
+mysqli_query($conexion,$sql);
+if (mysqli_errno($conexion)==0){
+	echo "Se ha añadido el usuario correctamente.<br>";
+	$num=mysqli_fetch_array($conexion->query("SELECT max(num_orden) as num_ord from encuesta"));
+	$norden = $num["num_ord"];
+	if(isset($lenguajes)){
+		foreach ($lenguajes as $i => $leng) {
+			$sql="INSERT INTO programa VALUES ('$norden','$leng')";
+			mysqli_query($conexion,$sql);
+			if (mysqli_errno($conexion)==0){echo "Lenguaje introducido<br>";}
+			else{echo "Error al introducir los lenguajes<br>";}
+		}
+	}
+	if(isset($idiomas)){
+		foreach ($idiomas as $i => $idiom) {
+			$sql="INSERT INTO habla VALUES ('$norden','$idiom')";
+			mysqli_query($conexion,$sql);
+			if (mysqli_errno($conexion)==0){echo "Idioma introducido<br>";}
+			else{echo "Error al introducir los idiomas<br>";}
+		}
+	}
+}
+else{ echo "Ha ocurrido un error.";}
+
 echo "</div>";
- 
 pie();
 ?>
